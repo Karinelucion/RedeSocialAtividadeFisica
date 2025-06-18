7076d1b02244b6a062d34f834d243ea6b726ed63
Karine Lucion <karinelucion04@gmail.com>
2025-06-18 19:47:16 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/redesocialatividadefisica/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.redesocialatividadefisica
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.credentials.Credential
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.lifecycle.lifecycleScope
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var credentialManager: CredentialManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
+        credentialManager = CredentialManager.create(this)
+
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btLoginOnClick(view: View) {
+        launchCredentialManager()
+    }
+
+    private fun launchCredentialManager(){
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setServerClientId(getString(R.string.defaut_web_client_id))
+            .setFilterByAuthorizedAccounts(false)
+            .build()
+
+        val  request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        lifecycleScope.launch {
+            try {
+                val result = credentialManager.getCredential(
+                    context = this@MainActivity,
+                    request = request
+                )
+                handleSignIn(result.credential)
+            }catch (e: Exception){
+                Log.e("Erro", "Não foi possível recuperar a credencial do usuário: ${e.localizedMessage}")
+            }
+        }
+    }
+
+    private fun handleSignIn(credential: Credential){
+        if(credential is CustomCredential && credential.type == TYPE_GOOGLE_ID_TOKEN_CREDENTIAL){
+            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+
+            firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+        }else{
+            Log.w("Erro", "Credencial não é do tipo Google ID!")
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    Log.d("Erro", "signInWithCredential:success")
+                    val email = auth.currentUser?.email
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Usuário: $email - $user", Toast.LENGTH_SHORT).show()
+                }else{
+                    Log.w("Erro", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+}
